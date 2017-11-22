94421f6d7515a45185baad49ce1ed66d015744e6
Jonathan <jonathanraes@gmail.com>
2017-11-22 11:03:24 +0100

    forward RFID scans aswell + forward over 2 ports

--- src/udpnode.py
+++ src/udpnode.py
@@ -6,17 +6,35 @@ import os
 import json
 from kt.msg import point
 from kt.msg import Cursor
+from std_msgs.msg import Int32
+from std_msgs.msg import UInt32
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
 
 # //define the display text
-def callback(data):
+def cursors_callback(data):
   rospy.loginfo("I receive %s", data.cursors)
-  sock.sendto(build_packet(data.cursors), (UDP_IP, UDP_PORT))
+  sock.sendto(build_points_packet(data.cursors), (UDP_IP, UDP_PORT))
+  sock.sendto(build_points_packet(data.cursors), (UDP_IP, UDP_PORT+1))
 
-def build_packet(points):
+def states_callback(data):
+  rospy.loginfo("I receive state change: %s", data.data)
+  sock.sendto(build_state_packet(data.data), (UDP_IP, UDP_PORT))
+  sock.sendto(build_state_packet(data.data), (UDP_IP, UDP_PORT+1))
+
+def rfid_callback(data):
+  rospy.loginfo("I receive rfid id: %s", data.data)
+  sock.sendto(build_rfid_packet(data.data), (UDP_IP, UDP_PORT))
+  sock.sendto(build_rfid_packet(data.data), (UDP_IP, UDP_PORT+1))
+
+def build_rfid_packet(rfid_id):
+  data = {}
+  data['rfid'] = rfid_id
+  return json.dumps(data)
+
+def build_points_packet(points):
   data = {}
   p = []
   for point in points:
@@ -26,9 +44,16 @@ def build_packet(points):
   data['cursors'] = p
   return json.dumps(data)
 
+def build_state_packet(state):
+  data = {}
+  data['state'] = state
+  return json.dumps(data)
+
 def random_subscriber():
   rospy.init_node('udpnode')
-  rospy.Subscriber('chatter',Cursor, callback)
+  rospy.Subscriber('kinect_touch',Cursor, cursors_callback)
+  rospy.Subscriber('states', Int32, states_callback)
+  rospy.Subscriber('RFID', UInt32, rfid_callback)
   rospy.spin()
 
 if __name__=='__main__':
